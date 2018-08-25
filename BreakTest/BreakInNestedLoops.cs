fcd7b755fb9b74386c5286ae620594ff840f78a5
Nathan Pabst <nathan.pabst@gmail.com>
2018-08-25 13:44:27 -0500

    added gitignore

---
+++ BreakTest/BreakInNestedLoops.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BreakTest
+{
+    class BreakInNestedLoops
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+            //outer loop
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                Console.WriteLine("num = {0}", numbers[x]);
+                //inner loop
+                for (int y = 0; y < letters.Length; y++)
+                {
+                    if (y == x)
+                    {
+                        //return control to outer loop
+                        break;
+                    }
+                    Console.Write(" {0}", letters[y]);
+                }
+                Console.WriteLine();
+            }
+            //keep the console open in debug mode. 
+            //Console.WriteLine("Press any key to exit");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ BreakTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BreakTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 5) {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
